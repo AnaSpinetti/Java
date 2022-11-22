e877b206da30b00ba50925bd98aa37ad647301f8
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-11-21 23:25:27 -0300

    Create desafio-multiplos.java

---
+++ desafio-multiplos.java
@@ -0,0 +1,26 @@
+// IMPORTANTE: Para ler os dados de entrada do usuário, utilize a classe Scanner, 
+// instanciando seu leitor da seguinte forma: "Scanner leitor = new Scanner(System.in);". 
+// Por outro lado, para imprimir suas saídas, utilize System.out.print ou System.out.println.  
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int A, N;
+
+        Scanner input = new Scanner(System.in);
+        A = input.nextInt();
+        N = input.nextInt();
+        int soma = 0;
+
+        //TODO:  Retorne o valor da soma de todos múltiplos de "A" até o seu limite "N".
+         for(int i = 1; i <= N; i++){
+            if(i % A == 0) {
+                soma += i; 
+            }
+        }
+
+        System.out.println(soma);
+
+    }
+}
